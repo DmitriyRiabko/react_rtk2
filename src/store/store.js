2e9bb56358b2aade0beab7add9c3aa33192072e6
vsegda_vinovat <riabko97@gmail.com>
2024-02-19 14:06:07 +0200

    added store

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit"
-import { reducer } from "./favorites/favorites"
+import { combineReducers, configureStore } from "@reduxjs/toolkit"
+import { reducer as favoritesReducer} from "./favorites/favorites.slice"
+
+
+const reducers = combineReducers({
+    favorites:favoritesReducer
+})
 
 
 export const store = configureStore({
-    reducer :{
-        reducer
-    },
+    reducer :reducers
 })
